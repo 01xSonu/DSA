29c34ac9d710f368cf94a2401d9c092ea57c5b12
Sonu Kumar <sonukr99770@gmail.com>
2026-09-06 17:06:30 +0530

    Time: 5 ms (10.97%) | Memory: 9.3 MB (82.85%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        map<char,char>mpp1;
+        map<char,char>mpp2;
+        for(int i=0;i<s.length();i++){
+            if(mpp1.find(s[i])!=mpp1.end()){
+                if(mpp1[s[i]]!=t[i]){
+                    return false;
+                }
+
+            }
+             if(mpp2.find(t[i])!=mpp2.end()){
+                if(mpp2[t[i]]!=s[i]){
+                    return false;
+                }
+
+            }
+            if(mpp1.find(s[i])==mpp1.end() && mpp2.find(t[i])==mpp2.end()){
+                mpp1[s[i]]=t[i];
+                mpp2[t[i]]=s[i];
+            }
+        }
+        return true;
+
+     
+            
+       
+        
+
+    }
+};
